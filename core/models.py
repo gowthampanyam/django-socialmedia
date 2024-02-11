cb4f8fa0265a264ad71d83cd31fe860211282632
Gowtham Panyam <gowtham.panyam@gmail.com>
2024-02-12 02:15:04 +0530

    models for core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_images', blank=True)
+    location = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_images', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    no_of_likes = models.IntegerField(default=0)
+    caption = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
